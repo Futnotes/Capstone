181c6c3bc4e38bdcc0c7173b0658fc149c5f81d5
Ben Jennings <benjennings2003@yahoo.com>
2013-12-14 19:27:37 +0000

    Added Create and Edit Team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -14,14 +14,38 @@ class TeamsController < ApplicationController
   end
 
   def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      flash[:notice] = "Team was created."
+      redirect_to @team
+    else
+      flash[:error] = "There was an error creating the team. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(team_params)
+      flash[:notice] = "Team was updated."
+      redirect_to @team
+    else
+      flash[:error] = "There was an error saving the team details. Please try again."
+      render :edit
+    end
   end
 
   private
 
   def team_params
-    params.require(:person).permit(:team_name, :team_name_short)
+    params.require(:team).permit(:team_name, :team_name_short)
   end
 end
