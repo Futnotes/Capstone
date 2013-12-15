e5de8af1c255d88bae0f47244e12170f4d32f6f0
Ben Jennings <benjennings2003@yahoo.com>
2013-12-15 23:05:44 +0000

    seed issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
 require 'faker'
 
+rand(3..5).times do
+  password = Faker::Lorem.characters(10)
+  u = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: password,
+    password_confirmation: password)
+  u.skip_confirmation!
+  u.save
+
+end
+
 rand(5..7).times do
   p = Team.create(team_name: Faker::Lorem.words(rand(1..2)).join(" "))
+
 end
 
 puts "Seed finished"
-puts "#{Team.count} seeds created"
+puts "#{Team.count} teams created"
+puts "#{User.count} users created"
+
