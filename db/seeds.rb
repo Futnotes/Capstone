6119e9aec477d5c2e4da83fe569fbd51aec40198
Ben Jennings <benjennings2003@yahoo.com>
2014-01-03 03:11:17 +0800

    seed file causing home and away teams to be identical

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,17 +21,22 @@ require 'faker'
     end
 end
 
+
+
 Team.all.each do |home_team|
   rand(3..4).times do
+    t = Team.create(
+      team_name: Faker::Lorem.words(rand(1..2)).join(" ")
+      )
     home_team.matches.create(
-      home_team: team,
-      away_team: Faker::Lorem.words(rand(1..2)).join(" "),
+      home_team_id: home_team.id,
+      away_team_id: t.id,
       kick_off: Time.now + rand(600..3153600)
       )
   end
 
   rand(11..16).times do
-    team.users.create(
+    home_team.users.create(
       name: Faker::Name.name,
       )
     TeamRole.last.update_attribute(:role, "player")
@@ -45,26 +50,41 @@ u = User.new(
   password_confirmation: 'helloworld')
 u.save
 
-t = u.teams.create(
+dummy_team = u.teams.create(
       team_name: "Fulham FC"
       )
       TeamRole.last.update_attribute(:role, "manager")
-t.save
+dummy_team.save
 
 rand(3..4).times do
-  t.matches.create(
-    team_two_name: Faker::Lorem.words(rand(1..2)).join(" "),
+  t = Team.create(
+    team_name: Faker::Lorem.words(rand(1..2)).join(" ")
+     )
+  dummy_team.matches.create(
+    home_team_id: dummy_team.id,
+    away_team_id: t.id,
     kick_off: Time.now + rand(600..3153600)
     )
 
   11.times do
-    t.users.create(
+    dummy_team.users.create(
       name: Faker::Name.name,
       )
     TeamRole.last.update_attribute(:role, "player")
   end
 end
 
+#Team.all.each do |home_team|
+  #rand(3..4).times do
+   # t = Team.create(
+    #  team_name: Faker::Lorem.words(rand(1..2)).join(" ")
+     # )
+   # home_team.matches.create(
+    #  home_team_id: home_team.id,
+     # away_team_id: t.id,
+      #kick_off: Time.now + rand(600..3153600)
+      #)
+ # end
  
 
 puts "Seed finished"
